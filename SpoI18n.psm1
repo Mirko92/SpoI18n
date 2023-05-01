3c8984cffdb3f8581c5d74cc617496cb05103e22
mirko.petrelli <mirko.petrelli@avanade.com>
2023-05-01 11:41:22 +0200

    0.1.6 - Registered argument completer

--- SpoI18n.psm1
+++ SpoI18n.psm1
@@ -7,6 +7,25 @@
 . $PSScriptRoot\ConvertFrom-SpoI18nCsv.ps1;
 . $PSScriptRoot\Copy-SpoI18nCsv.ps1;
 
-import-module $PSScriptRoot\utils\Locales.ps1;
-
 Export-ModuleMember -Function Get-SpoI18nCsv, ConvertFrom-SpoI18nCsv, Copy-SpoI18nCsv, LocaleArgumentCompleter ;
+
+$localesJsonPath = "$PSScriptRoot\assets\Locales.json";
+
+$scriptblock = {
+  param ( 
+    $commandName,
+    $parameterName,
+    $wordToComplete,
+    $commandAst,
+    $fakeBoundParameters 
+  )
+
+  $possibleValues = Get-Content -Path $localesJsonPath | ConvertFrom-Json;
+
+  $possibleValues = $possibleValues | ForEach-Object { $_.label };
+
+  $possibleValues | Where-Object { $_ -like "$wordToComplete*" };
+}
+
+Register-ArgumentCompleter -CommandName Get-SpoI18nCsv          -ParameterName Locales       -ScriptBlock $scriptBlock;
+Register-ArgumentCompleter -CommandName ConvertFrom-SpoI18nCsv  -ParameterName LocalesToSkip -ScriptBlock $scriptBlock;
