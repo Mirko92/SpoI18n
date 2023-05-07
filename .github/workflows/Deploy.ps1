989ca745021703ddd4c628fac8e20e9869817fb8
mirko.petrelli <mirko.petrelli@avanade.com>
2023-05-07 12:56:29 +0200

    Update deploy script

---
+++ .github/workflows/Deploy.ps1
@@ -0,0 +1,33 @@
+param (
+  [Parameter(Mandatory)]
+  [string] $nugetApiKey
+)
+
+$moduleName   = "SpoI18n";
+$manifestPath = "./SpoI18n.psd1";
+
+# Get last PSGallery module version
+$remoteVersion = (Find-Module  -Name $moduleName `
+                              -Repository PSGallery).Version;
+
+Write-Host "PSGallery version: $remoteVersion";
+
+# Get powershell metadata
+$manifest = Import-PowerShellDataFile $manifestPath;
+
+# Get current version [string]
+$currentVersion = $manifest.ModuleVersion;
+Write-Host "Current version: $currentVersion";
+
+# If current version is greater than remote version, it'll be published
+if ( $currentVersion.CompareTo($remoteVersion) -gt 0 ) {
+  Write-Host "Current version is greater than remote version";
+
+  Publish-Module -Path ./ -NuGetApiKey $nugetApiKey;
+
+  return 0;
+} else {
+  Write-Host "Current version is less or equal to remote version. No need to deploy."
+
+  return 1;
+}
